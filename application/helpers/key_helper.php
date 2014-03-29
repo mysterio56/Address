26c6f6174f8e0c2db92ebc714b1406aae0791a15
mysterio56 <aslanlion56@gmail.com>
2014-03-28 23:35:41 -0600

    arreglo nuevo

--- application/helpers/key_helper.php
+++ application/helpers/key_helper.php
@@ -1,27 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 if ( ! function_exists('keyPass'))
-{
-    function keyPass(){
-
-    	if(isset($_POST)){
-
-	        if ($_POST['key'] != '1owvUtwudHylyenow15OkerirfudhocOve'){
-
-	            echo json_encode(array('error'=>'Acceso denegado!!!'));
-
-	            exit();
-
-	        }
-
-    	} else {
-
-    		echo json_encode(array('error'=>'Acceso denegado!!!'));
-
-	        exit();
-
-    	}
-
-    }
-
-}   
+ {
+     function keyPass(){
+ 
+         $aHeaders = getallheaders();
+ 
+         if (@$aHeaders['key'] != '1owvUtwudHylyenow15OkerirfudhocOve'){
+ 
+             echo json_encode(array('error'=>'Acceso denegado!!!'));
+ 
+             exit();
+ 
+         }
+ 
+     }
+ 
+ }  
